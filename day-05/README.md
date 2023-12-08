225776d2c3e0677a69c3a1f7ade3a9d475e8dd0a
Mike Rivnak <rivnakm1@gmail.com>
2023-12-08 16:31:31 -0500

    day 5 part 2

--- day-05/README.md
+++ day-05/README.md
@@ -68,6 +68,7 @@ Any source numbers that aren't mapped correspond to the same destination number.
 
 So, the entire list of seed numbers and their corresponding soil numbers looks like this:
 
+```
 seed  soil
 0     0
 1     1
@@ -81,18 +82,41 @@ seed  soil
 97    99
 98    50
 99    51
+```
 With this map, you can look up the soil number required for each initial seed number:
 
-Seed number 79 corresponds to soil number 81.
-Seed number 14 corresponds to soil number 14.
-Seed number 55 corresponds to soil number 57.
-Seed number 13 corresponds to soil number 13.
+- Seed number 79 corresponds to soil number 81.
+- Seed number 14 corresponds to soil number 14.
+- Seed number 55 corresponds to soil number 57.
+- Seed number 13 corresponds to soil number 13.
+
 The gardener and his team want to get started as soon as possible, so they'd like to know the closest location that needs a seed. Using these maps, find the lowest location number that corresponds to any of the initial seeds. To do this, you'll need to convert each seed number through other categories until you can find its corresponding location number. In this example, the corresponding types are:
 
-Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
-Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
-Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
-Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
+- Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
+- Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
+- Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
+- Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
+
 So, the lowest location number in this example is 35.
 
 What is the lowest location number that corresponds to any of the initial seed numbers?
+
+Your puzzle answer was 178159714.
+
+The first half of this puzzle is complete! It provides one gold star: *
+
+## Part Two
+Everyone will starve if you only plant such a small number of seeds. Re-reading the almanac, it looks like the seeds: line actually describes ranges of seed numbers.
+
+The values on the initial seeds: line come in pairs. Within each pair, the first value is the start of the range and the second value is the length of the range. So, in the first line of the example above:
+
+seeds: 79 14 55 13
+This line describes two ranges of seed numbers to be planted in the garden. The first range starts with seed number 79 and contains 14 values: 79, 80, ..., 91, 92. The second range starts with seed number 55 and contains 13 values: 55, 56, ..., 66, 67.
+
+Now, rather than considering four seed numbers, you need to consider a total of 27 seed numbers.
+
+In the above example, the lowest location number can be obtained from seed number 82, which corresponds to soil 84, fertilizer 84, water 84, light 77, temperature 45, humidity 46, and location 46. So, the lowest location number is 46.
+
+Consider all of the initial seed numbers listed in the ranges on the first line of the almanac. What is the lowest location number that corresponds to any of the initial seed numbers?
+
+Your puzzle answer was 100165128.
--- day-05/part-2/src/main.rs
+++ day-05/part-2/src/main.rs
@@ -1,53 +1,173 @@
-use std::collections::HashSet;
+use std::ops::RangeInclusive;
+use regex::Regex;
+use strum_macros::EnumString;
 
-fn main() {
-    let input = include_str!("../../input.txt").lines();
-
-    let results = input
-        .map(|line| {
-            let mut is_winning_nums = false;
-            let mut winning_nums = HashSet::new();
-            let mut nums = HashSet::new();
-            let mut current = 0;
-            for (i, c) in line.chars().enumerate() {
-                if c.is_digit(10) {
-                    current = current * 10 + c.to_digit(10).unwrap();
-                } else if c == ':' {
-                    is_winning_nums = true;
-                    current = 0;
-                } else if c == '|' {
-                    is_winning_nums = false;
-                }
-                if c == ' ' || i == line.len() - 1 {
-                    if current == 0 {
-                        continue;
-                    }
-                    if is_winning_nums {
-                        winning_nums.insert(current);
-                    } else {
-                        nums.insert(current);
-                    }
-                    current = 0;
-                }
-            }
-
-            nums.intersection(&winning_nums).count() as u32
-        })
-        .collect::<Vec<u32>>();
-
-    let sum = results
-        .iter()
-        .enumerate()
-        .map(|(i, _)| get_copies(&results, i))
-        .sum::<u32>();
-
-    println!("{}", sum);
+#[derive(Clone, Debug, EnumString, Eq, PartialEq)]
+#[strum(serialize_all = "snake_case")]
+enum Item {
+    Seed,
+    Soil,
+    Fertilizer,
+    Water,
+    Light,
+    Temperature,
+    Humidity,
+    Location,
 }
 
-fn get_copies(results: &Vec<u32>, index: usize) -> u32 {
-    let mut sum = 1;
-    for i in 1..=results[index] {
-        sum += get_copies(results, index + i as usize);
+#[derive(Debug)]
+struct MapRange {
+    src: RangeInclusive<u64>,
+    dest: RangeInclusive<u64>,
+}
+
+impl MapRange {
+    fn new(dest_start: u64, src_start: u64, length: u64) -> Self {
+        Self { src: src_start..=src_start + length - 1, dest: dest_start..=dest_start + length - 1 }
     }
-    sum
+}
+
+#[derive(Debug)]
+struct Map {
+    from: Item,
+    to: Item,
+    ranges: Vec<MapRange>,
+}
+
+impl Map {
+    fn is_mapped(&self, seed: u64) -> bool {
+        self.ranges.iter().any(|r| r.src.contains(&seed))
+    }
+
+    fn map(&self, seed: u64) -> u64 {
+        if !self.is_mapped(seed) {
+            return seed;
+        }
+        // TODO: could use binary search here
+        let range = self.ranges.iter().find(|r| r.src.contains(&seed)).unwrap();
+        let index = seed - range.src.start();
+        range.dest.start() + index
+    }
+}
+
+#[derive(Debug)]
+struct Almanac {
+    seeds: Vec<RangeInclusive<u64>>,
+    maps: Vec<Map>,
+}
+
+impl Almanac {
+    fn get_map(&self, from: &Item) -> Option<&Map> {
+        self.maps.iter().find(|m| m.from.clone() == *from)
+    }
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let almanac = build_almanac(input);
+    let min = almanac.seeds.iter().flat_map(|r| r.clone()).map(|seed| {
+        let seed_map = almanac.get_map(&Item::Seed).expect("No seed map found");
+        map_item(seed, seed_map, &almanac)
+    }).min().expect("No min seed found");
+    println!("{}", min);
+}
+
+fn map_item(item: u64, map: &Map, almanac: &Almanac) -> u64 {
+    match map.to {
+        Item::Location => map.map(item),
+        _ => {
+            let next_map = almanac.get_map(&map.to).expect("No map found");
+            map_item(map.map(item), next_map, almanac)
+        }
+    }
+}
+
+fn build_almanac(input: &str) -> Almanac {
+    let seeds_re = Regex::new(r"seeds:\s(\d+(?:\s\d+)*)").expect("Invalid seeds regex");
+    let seeds = seeds_re
+        .captures(input)
+        .expect("No seeds found")
+        .get(1)
+        .expect("No seeds found")
+        .as_str()
+        .split_whitespace()
+        .map(|s| s.parse::<u64>().expect("Invalid seed"))
+        .collect::<Vec<u64>>();
+    let seed_ranges = seeds.chunks_exact(2).map(|c| c[0]..=c[0]+c[1]-1).collect::<Vec<_>>();
+
+    let maps_re = Regex::new(r"(.*-to-.*) map:\n((?:\d+\s\d+\s\d+\n?)+)").expect("Invalid maps regex");
+    let map_name_re = Regex::new(r"(.*)-to-(.*)").expect("Invalid map name regex");
+    let maps = maps_re
+        .captures_iter(input)
+        .map(|cap| {
+            let name = cap.get(1).expect("No map name found").as_str();
+            let name_cap = map_name_re.captures(name).expect("Invalid map name");
+
+            let from = name_cap.get(1).expect("No map from found").as_str().parse::<Item>().expect("Invalid map from");
+            let to = name_cap.get(2).expect("No map to found").as_str().parse::<Item>().expect("Invalid map to");
+
+            let map = cap.get(2).expect("No map found").as_str();
+            let map = map.lines().map(|l| {
+                let mut parts = l.split_whitespace();
+                let dest_start = parts.next().expect("No map dest start found").parse::<u64>().expect("Invalid map dest start");
+                let src_start = parts.next().expect("No map src start found").parse::<u64>().expect("Invalid map src start");
+                let length = parts.next().expect("No map length found").parse::<u64>().expect("Invalid map length");
+                MapRange::new(dest_start, src_start, length)
+            });
+            Map { from, to, ranges: map.collect::<Vec<_>>() }
+        })
+        .collect::<Vec<_>>();
+
+    Almanac { seeds: seed_ranges, maps }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_map_is_mapped() {
+        let map = Map {
+            from: Item::Seed,
+            to: Item::Soil,
+            ranges: vec![MapRange::new(100, 0, 10)],
+        };
+        assert!(map.is_mapped(0));
+        assert!(map.is_mapped(9));
+        assert!(!map.is_mapped(10));
+    }
+
+    #[test]
+    fn test_map_map() {
+        let map = Map {
+            from: Item::Seed,
+            to: Item::Soil,
+            ranges: vec![MapRange::new(100, 0, 10)],
+        };
+        assert_eq!(map.map(0), 100);
+        assert_eq!(map.map(9), 109);
+        assert_eq!(map.map(20), 20);
+    }
+
+    #[test]
+    fn test_almanac_get_map() {
+        let map0 = Map {
+            from: Item::Seed,
+            to: Item::Soil,
+            ranges: vec![MapRange::new(100, 0, 10)],
+        };
+        let map1 = Map {
+            from: Item::Humidity,
+            to: Item::Location,
+            ranges: vec![MapRange::new(100, 0, 10)],
+        };
+
+        let almanac = Almanac {
+            seeds: vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            maps: vec![map0, map1],
+        };
+        let map = almanac.get_map(&Item::Humidity).expect("No map found");
+        assert!(matches!(map.from, Item::Humidity));
+    }
 }
